14479dbc971c7455948e216a1a2c5e65f0f1a367
Rob Snow <robjsnow@gmail.com>
2024-06-24 00:26:14 -0500

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/(.*)',
+          has: [
+            {
+              type: 'host',
+              value: 'nwaspot.com', 
+            },
+          ],
+          permanent: true,
+          destination: 'https://www.nwaspot.com/$1',
+        },
+        {
+          source: '/(.*)',
+          has: [
+            {
+              type: 'host',
+              value: 'www.nwaspot.com', 
+            },
+          ],
+          permanent: true,
+          destination: 'https://www.nwaspot.com/$1',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
